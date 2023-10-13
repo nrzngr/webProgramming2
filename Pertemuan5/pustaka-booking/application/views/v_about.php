f7f860ef4f6e6a7cf1e99b40d7da44adb06e4468
nrzngr <109433343+nrzngr@users.noreply.github.com>
2023-10-13 16:47:13 +0700

    Add files via upload

---
+++ Pertemuan5/pustaka-booking/application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1>
+        <?= $judul; ?>
+    </h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan   : Ridzki</li>
+        <li>Nama Belakang: Nugraha</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl. Mana</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>New York</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Rebahan</li>
+        <li>Esports</li>
+    </ul>
+</section>
